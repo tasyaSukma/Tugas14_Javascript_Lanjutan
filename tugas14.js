2a22ccec5daec525bbee4af53fa45e545611b217
tasyaSukma <maftuhah.tasya@gmail.com>
2020-09-21 08:59:20 +0700

    Add files via upload

---
+++ tugas14.js
@@ -0,0 +1,15 @@
+var bagiLahir = 37/21
+
+console.log(bagiLahir)
+
+console.log("Fungsi Math.round")
+var value = Math.round(bagiLahir)
+console.log(value)
+
+console.log("Fungsi Math.ceil")
+var value = Math.ceil(bagiLahir)
+console.log(value)
+
+console.log("Fungsi Math.floor")
+var value = Math.floor(bagiLahir)
+console.log(value)
